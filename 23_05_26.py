8426702d8dcefba8daccb0affea24c407f12416d
Abdulloh Solihov <abdullohsolihov@gmail.com>
2026-05-25 05:48:42 +0500

    boldi

---
+++ 23_05_26.py
@@ -0,0 +1,54 @@
+#11111111111111111111111111111111111111
+
+# def True_False(lst:list):
+#     counts=[lst.count(x) for x in set(lst)]
+#     print(len(counts)==len(set(counts)))
+
+
+
+# n=int(input("Nechta son"))
+# lst=[]
+#print("Sonlarni yozing")
+# for i in range(n):
+#     a=int(input())
+#     lst.append(a)
+# True_False(lst)
+
+
+
+#222222222222222222222222222222222222222222222222222222
+
+# def raqamlar(word):
+#     raqam=""
+#     natija=set()
+#     for i in word:
+#         if i.isdigit():
+#             raqam+=i
+#         else:
+#             if raqam:
+#                 natija.add(int(raqam))
+#                 raqam=""
+#     if raqam:
+#         natija.add(int(raqam))
+#     return len(natija)
+
+
+# word=input("Yozing:")
+# print(raqamlar(word))
+
+
+
+#3333333333333333333333333333333333333333333
+
+# def nechta_katta(lst:list):
+#     print([sum(j<i for j in lst)for i in lst])
+
+# n=int(input("nechta son:"))
+# nums=[]
+# print("sonlarni kiriting:")
+# for i in range(n):
+#     a=int(input())
+#     nums.append(a)
+# nechta_katta(nums)
+
+
